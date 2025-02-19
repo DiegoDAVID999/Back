a7156e62b6e6fd25184076a3cdb2d8861722621b
DiegoDAVID <alejandroreal7771@gmail.com>
2025-02-18 19:56:47 -0500

    new committ

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ class Server {
   middlewares() {
     this.app.use(
       cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173", // Asegúrate de que esta URL coincida con la de tu frontend
+        origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(",") : ["http://localhost:5173"],
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        allowedHeaders: ["Content-Type", "Authorization"],
       })
     );
     this.app.use(express.json());
