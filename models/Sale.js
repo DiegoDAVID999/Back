74a191d9c9b614bcee8a79fb093e257637a92331
DiegoDAVID <alejandroreal7771@gmail.com>
2025-03-03 18:50:55 -0500

    Men777

--- models/Sale.js
+++ models/Sale.js
@@ -1,11 +1,33 @@
 
 
 
-import mongoose from "mongoose"
-import moment from "moment-timezone"
+// import mongoose from "mongoose"
+// import moment from "moment-timezone"
 
 
 
+
+// const saleSchema = new mongoose.Schema({
+//   products: [
+//     {
+//       product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+//       customId: { type: String, required: true },
+//       name: { type: String, required: true },
+//       price: { type: Number, required: true },
+//       quantity: { type: Number, required: true },
+//     },
+//   ],
+//   total: { type: Number, required: true },
+//   date: { 
+//     type: Date, 
+//     default: () => moment().tz("America/Bogota").toDate() 
+//   },
+// })
+
+// export default mongoose.model("Sale", saleSchema)
+
+import mongoose from "mongoose";
+import moment from "moment-timezone";
 
 const saleSchema = new mongoose.Schema({
   products: [
@@ -18,11 +40,11 @@ const saleSchema = new mongoose.Schema({
     },
   ],
   total: { type: Number, required: true },
-  date: { 
-    type: Date, 
-    default: () => moment().tz("America/Bogota").toDate() 
+  paymentMethod: { type: String, required: true },  // Aquí agregamos el método de pago
+  date: {
+    type: Date,
+    default: () => moment().tz("America/Bogota").toDate(),
   },
-})
+});
 
-export default mongoose.model("Sale", saleSchema)
-
+export default mongoose.model("Sale", saleSchema);
